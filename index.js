da25dc9f31c56c3c8e7afd3d6d891826f7daa767
CS-Sudhanshu <Sudhanshu.deb@91coderstreet.com>
2025-03-14 22:51:07 +0530

    EXpress file

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/welcome", (req, res) => {
+  res.render("welcome");
+});
+
+// Make sure this port is available!
+app.listen(4000);
